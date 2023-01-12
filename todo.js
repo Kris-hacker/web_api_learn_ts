79cb0382c05e36f5a84fce7e474400680950b87c
hokaichun <b33442324@gmail.com>
2023-01-12 20:53:39 +0800

    update ans in todo

--- todo.js
+++ todo.js
@@ -7,13 +7,30 @@ do {
     input = String(readline.question('enter command: ')).trim();
     if (input.indexOf('add ') === 0) {
         var space = input.indexOf(' ');
-        var item = input.substring(space).trim();
-        console.log("adding \"".concat(item, "\""));
-        items.push(item);
+        var item = input.substring(space).trim().toLowerCase();
+        if (items.indexOf(item) >= 0) {
+            console.log("".concat(item, " is already existed"));
+        }
+        else {
+            console.log("adding \"".concat(item, "\""));
+            items.push(item);
+        }
     }
     if (input.indexOf('list') === 0) {
         for (var i = 0; i < items.length; i++) {
             console.log("".concat(i, ". ").concat(items[i]));
+        }
+    }
+    if (input.indexOf("remove") === 0) {
+        var space = input.indexOf(' ');
+        var item = input.substring(space).trim().toLowerCase();
+        var itemIdx = items.indexOf(item);
+        if (itemIdx >= 0) {
+            console.log("removing ".concat(item));
+            items.splice(itemIdx, 1);
+        }
+        else {
+            console.log("".concat(item, " is not found."));
         }
     }
 } while (input !== 'exit');
--- todo.ts
+++ todo.ts
@@ -8,13 +8,30 @@ do {
 	input = String(readline.question('enter command: ')).trim()
 	if (input.indexOf('add ') === 0) {
 		const space: number = input.indexOf(' ')
-		const item: string = input.substring(space).trim()
-		console.log(`adding "${item}"`)
-		items.push(item)
+		const item: string = input.substring(space).trim().toLowerCase()
+		
+		if(items.indexOf(item) >= 0) {
+			console.log(`${item} is already existed`);
+		} else {
+			console.log(`adding "${item}"`);
+			items.push(item);
+		}
 	}
 	if (input.indexOf('list') === 0) {
 		for (let i=0; i< items.length; i++) {
 			console.log(`${i}. ${items[i]}`)
+		}
+	}
+	if (input.indexOf(`remove`) === 0){
+		const space: number = input.indexOf(' ');
+		const item: string = input .substring(space).trim().toLowerCase();
+		const itemIdx: number = items.indexOf(item);
+
+		if(itemIdx >= 0){
+			console.log(`removing ${item}`);
+			items.splice(itemIdx,1);
+		} else{
+			console.log(`${item} is not found.`);
 		}
 	}
 } while (input !== 'exit')
